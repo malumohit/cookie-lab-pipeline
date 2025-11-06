774db7bf37408bb172666fa42a37ef6ac92229c3
Kartiki Pande <kpande3289@sdsu.edu>
2025-11-05 16:53:00 -0800

    Update launch_chrome_with_ext.bat

--- launch_chrome_with_ext.bat
+++ launch_chrome_with_ext.bat
@@ -1,4 +1,4 @@
 @echo off
 set EXT_DIR=C:\cookie-lab\extensions\chromium\honey old
 set PROFILE=C:\cookie-lab\pptr_profile
-start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --user-data-dir="%PROFILE%" --load-extension="%EXT_DIR%" --disable-notifications --no-first-run --no-default-browser-check "https://www.bestbuy.com"
+start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --user-data-dir="%PROFILE%" --disable-notifications --no-first-run --no-default-browser-check "https://www.bestbuy.com"
